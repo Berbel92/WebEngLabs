274318d8510b95376d8c876a1a18aad4531c301b
628204 <alberto_b_a@msn.com>
2014-11-02 17:59:00 +0100

    ListTasks.java

---
+++ ToDo/src/main/java/toDo/ListTasks.java
@@ -0,0 +1,32 @@
+package toDo;
+
+import java.io.FileReader;
+
+import com.google.gson.Gson;
+
+public class ListTasks {
+
+	public final static String DEFAULT_FILE_NAME = "toDo_tasks.json";
+
+	static void Print(TasksList tasksList) {
+		for (Task task : tasksList.getTasksList()) {
+			System.out.println("\nTask name: " + task.getTask());
+			System.out.println("     context: " + task.getContext());
+			System.out.println("     project: " +  task.getProject());
+			System.out.println("     priority(0-10): " + task.getPriority() + "\n");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Gson gson = new Gson();
+		String filename = DEFAULT_FILE_NAME;
+		if (args.length > 0) {
+			filename = args[0];
+		}
+
+		TasksList tasksList = gson.fromJson(new FileReader(filename),
+				TasksList.class);
+
+		Print(tasksList);
+	}
+}
